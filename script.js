2fbae0e41e66aec35b6930a95d32bc84a11002d1
hasankoroglu <nurcihanunsur@gmail.com>
2022-10-04 22:05:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const btnEl = document.querySelector(".btn");
+const closeIconEL = document.querySelector(".close-icon");
+const trailerContainerEl = document.querySelector(".trailer-container");
+const videoEl = document.querySelector("video");
+btnEl.addEventListener("click", () => {
+    trailerContainerEl.classList.remove("active");
+
+});
+
+closeIconEL.addEventListener("click", () => {
+    trailerContainerEl.classList.add("active");
+    videoEl.pause();
+    videoEl.currenTime = 0;
+
+});
